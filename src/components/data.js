05d48d65a8b3f10c59034892197a8cbfc415a093
darriusisart <110921782+darriusisart@users.noreply.github.com>
2024-11-10 12:53:27 -0600

    data.js for course and deadlines data

---
+++ src/components/data.js
@@ -0,0 +1,33 @@
+export const deadlinesData = {
+    "ABC1000": [
+        { id: 1, dateDue: "2024-11-15", timeDue: "23:59", course: "Course A", assignment: "Homework 3", submitted: false },
+    ],
+    "ABC2345": [
+        { id: 2, dateDue: "2024-11-18", timeDue: "10:00", course: "Course B", assignment: "Essay Draft", submitted: true },
+    ],
+    "ABC3333": [
+        { id: 3, dateDue: "2024-11-20", timeDue: "17:00", course: "Course C", assignment: "Lab Report", submitted: false },
+    ],
+    "ABC1111": [
+        { id: 4, dateDue: "N/A", timeDue: "N/A", course: "Course D", assignment: "There are no deadlines for this class!", submitted: true},
+    ],
+};
+
+export const courseData = {
+    "ABC1000": [
+        { assignment: "Homework 1", grade: "A" },
+        { assignment: "Quiz 1", grade: "B" },
+    ],
+    "ABC2345": [
+        { assignment: "Essay Draft", grade: "A-" },
+        { assignment: "Project 1", grade: "B+" },
+    ],
+    "ABC3333": [
+        { assignment: "Lab", grade: "A" },
+        { assignment: "Project 1", grade: "B-" },
+    ],
+    "ABC1111": [
+        { assignment: "Program 0", grade: "B+" },
+        { assignment: "Project 1", grade: "B+" },
+    ],
+}
