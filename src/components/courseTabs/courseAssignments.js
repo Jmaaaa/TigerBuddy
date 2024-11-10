3c86595ab0350b1e95251cf3ffd4639ff29b9fb2
darriusisart <110921782+darriusisart@users.noreply.github.com>
2024-11-10 12:55:17 -0600

    correct info in course's grades and assignments tab

--- src/components/courseTabs/courseAssignments.js
+++ src/components/courseTabs/courseAssignments.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { deadlinesData } from "../../components/data.js";
 
 const CourseAssignments = () => {
-    
+    const { name: courseName } = useParams();
+    const assignments = deadlinesData[courseName] || [];
+
     return (
-        <h1>
-            Welcome to the assignments
-        </h1>
+        <div>
+            <h1>Assignments for {courseName}</h1>
+            <ul>
+                {assignments.map((assignment) => (
+                    <li key={assignment.id}>
+                        <strong>{assignment.assignment}</strong> - Due: {assignment.dateDue} at {assignment.timeDue} 
+                        <span> - {assignment.submitted ? "Submitted" : "Not Submitted"}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
--- src/components/courseTabs/courseGrades.js
+++ src/components/courseTabs/courseGrades.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { useOutletContext } from 'react-router-dom';
 
 const CourseGrades = () => {
+    const { courseGrades } = useOutletContext();
     
     return (
-        <h1>
-            Welcome to the grades
-        </h1>
+       <div>
+            <h1>Welcome to the Grades</h1>
+            {courseGrades.length > 0 ? (
+                <ul>
+                    {courseGrades.map((grade, index) => (
+                        <li key={index}>
+                            {grade.assignment}: {grade.grade}
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No grades available for this course.</p>
+            )}
+        </div>
     );
 };
 
