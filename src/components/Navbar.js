205647fbf66eb2e2bf8be6e86d334c0a62cf79e8
jmayeLSU <jmaye34@lsu.edu>
2024-11-10 15:08:17 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link, Outlet } from "react-router-dom";
 const Navbar = () => {
     return (
         <>
-            <div className="d-flex p-2 bg-purple">
+            <div className="d-flex p-2 bg-purple sticky-top">
                 <h1 className="text-white">Tiger Buddy</h1>
                 <div className="d-flex ml-auto">
                     <Link to="/dashboard" className="m-2 p-2 text-white">Dashboard</Link>
