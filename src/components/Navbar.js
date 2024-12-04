a60e51343eabaded29ea71f11d776dafd1e8039a
Jmaaaa <jakemayer365@gmail.com>
2024-12-04 07:52:59 -0600

    touch ups

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,13 @@ const Navbar = () => {
     return (
         <>
             <div className="navbar navbar-expand-lg bg-primary sticky-top">
-                <div className="container-fluid">
+                <div className="container-fluid d-flex">
                     <a className="navbar-brand d-flex align-items-center" href="#/dashboard">
                         <span className="h1 fw-bolder ms-5"><span className="text-white">Tiger</span><span className="color-gold">Buddy</span></span>
                         <img src={logo512} width="64" height="64" alt="logo" className="ms-2"></img>
                     </a>
 
-                    <ul className="navbar-nav ms-auto me-5">
+                    <ul className="navbar-nav ms-auto me-4 d-flex flex-row gap-3 ">
                         <li class="nav-item">
                             <Link to="/dashboard" className="nav-link text-white">Dashboard</Link>
                         </li>
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -27,43 +27,42 @@ const Dashboard = () => {
     
 
     return (
-        <div className="d-flex flex-column h-100">
-            <div className=" d-flex align-items-center bg-light" 
-            style={{height: "15%"}}>
-                <h1 className="mx-5">
+        <div className="d-flex flex-column flex-fill">
+            <div className="container-fluid p-4 ps-5 d-flex align-items-center bg-light">
+                <h1>
                     Welcome to the dashboard
                 </h1>
             </div>
-            <div className="d-flex flex-row p-3 " style={{height: "80%"}}>
-                <div className="d-flex flex-column w-25">
+            <div className="d-flex flex-row flex-wrap">
+                <div className=" d-flex flex-column ms-4" style={{minWidth: "20rem", minHeight: "40rem"}}>
                     {courses.map((course,index) => (
-                        <CourseCard key={index} course={course} userId={userId}/>
+                        <CourseCard key={index} course={course} userId={userId} className="flex-fill"/>
                     ))}
                 </div>
-                <div className="d-flex flex-column px-3 w-100">
-                    <div className="flex-grow-1 d-flex flex-row h-50 mb-1">
-                        <div className="w-50 d-flex flex-column">
+                <div className="d-flex flex-column col">
+                    <div className="d-flex flex-row flex-wrap">
+                        <div className="container m-3 d-flex flex-column col " style={{minWidth: "20rem", maxHeight: "30rem"}}>
                             <h2 className="m-2">
                             Announcements
                             </h2>
-                            <div className="p-2 d-flex flex-column overflow-auto border-top border-bottom">
+                            <div className="p-2 d-flex flex-column flex-fill overflow-auto border-top border-bottom">
                                 <AnnouncementCard/>
                             </div>
                         </div>
-                        <div className="w-50 ps-4 d-flex flex-column">
+                        <div className="container m-3 d-flex flex-column col" style={{minWidth: "20rem", maxHeight: "30rem"}}>
                             <h2 className="m-2">
                             Feedback
                             </h2>
-                            <div className="p-2 d-flex flex-column overflow-auto border-top border-bottom">
+                            <div className="p-2 d-flex flex-column  flex-fill overflow-auto border-top border-bottom">
                                 <FeedbackCard/>
                             </div>
                         </div>
                     </div>
-                    <div className="flex-shrink-0 mt-1">
+                    <div className="container d-flex flex-column flex-shrink-0 mt-1">
                         <h2 className="m-2 mb-3">
                         Upcoming Schedule
                         </h2>
-                        <div>
+                        <div className="container d-flex flex-column">
                             <CalendarPreview/>
                         </div>
                     </div>
