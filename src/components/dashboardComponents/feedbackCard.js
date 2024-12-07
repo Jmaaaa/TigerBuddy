6398fae3c0f6c2178024df1b07d9a126e8e28c95
Jmaaaa <jakemayer365@gmail.com>
2024-12-07 11:46:52 -0600

    Update feedbackCard.js

--- src/components/dashboardComponents/feedbackCard.js
+++ src/components/dashboardComponents/feedbackCard.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 // import { assignmentData, nameData, profData } from "../data";
 import feedbackImg from "../../assets/feedback.png";
 import { getLetterGrade } from "../../pages/grades";
+import { Link } from "react-router-dom";
 
 const FeedbackCard = ({ feedback }) => {
     // const [gotFeedback, setGotFeedback] = useState(false);
@@ -33,19 +34,20 @@ const FeedbackCard = ({ feedback }) => {
             {feedback.map((data, idx) => {
                 console.log(JSON.stringify(data));
                 return (
-                    <div className="d-flex flex-row w-100 p-3 mb-2 bg-light bg-gradient rounded" key={idx}>
-                        <div><img src={feedbackImg} alt="feedback-icon" width="32" className="rounded"/></div>
-                        <div className="d-flex flex-column ms-3">
-                            <h5 className="mb-1">{data.name}</h5>
-                            <h6 className="mb-1">2024 {data.courseName} for {data.courseProf}</h6>
-                            <span className="d-flex flex-row">
-                                <p className="small my-0">{data.grade.score} ({getLetterGrade(data.grade.score)})</p>
-                                {/* <p className="small ms-1 my-0">{data.text}</p> */}
-                                <p className="small ms-3 my-0">{data.grade.feedback}</p>
-                            </span>
-                            
-                        </div>
-                    </div>
+                        <Link to={`../courses/${data.course}/assignments/${data.name}`} 
+                            className="d-flex flex-row w-100 p-3 mb-2 bg-light bg-gradient rounded text-decoration-none text-dark" key={idx}>
+                            <div><img src={feedbackImg} alt="feedback-icon" width="32" className="rounded"/></div>
+                            <div className="d-flex flex-column ms-3">
+                                <h5 className="mb-1">{data.name}</h5>
+                                <h6 className="mb-1">2024 {data.courseName} for {data.courseProf}</h6>
+                                <span className="d-flex flex-row">
+                                    <p className="small my-0">{data.grade.score} ({getLetterGrade(data.grade.score)})</p>
+                                    {/* <p className="small ms-1 my-0">{data.text}</p> */}
+                                    <p className="small ms-3 my-0">{data.grade.feedback}</p>
+                                </span>
+                                
+                            </div>
+                        </Link>
                 );
             })}
         </div>
